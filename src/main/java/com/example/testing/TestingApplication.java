e3d0434bae73461c4ba8c13798627387440639ec
alexandrafirsova <alsfirsova@gmail.com>
2023-08-10 15:07:13 +0300

    first commit

---
+++ src/main/java/com/example/testing/TestingApplication.java
@@ -0,0 +1,13 @@
+git remote add origin https://github.com/AleksandraFirsova/TestingSkyPro.gitpackage com.example.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingApplication.class, args);
+	}
+
+}
